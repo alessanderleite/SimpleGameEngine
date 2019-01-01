12cc5d3080e46b36c83fbe6e6ce9daf3c5b6466e
alessanderleite <alessander.logistica@gmail.com>
2019-01-01 11:19:33 -0200

    the inner class called GameView

--- app/src/main/java/br/com/alessanderleite/simplegameengine/SimpleGameEngine.java
+++ app/src/main/java/br/com/alessanderleite/simplegameengine/SimpleGameEngine.java
@@ -1,12 +1,30 @@
 package br.com.alessanderleite.simplegameengine;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.SurfaceView;
 
 public class SimpleGameEngine extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    // Here is our implementation of GameView
+    // It is an inner class.
+    // Note how the final closing curly brace }
+    // is inside SimpleGameEngine
+    class GameView extends SurfaceView implements Runnable {
+
+        public GameView(Context context) {
+            super(context);
+        }
+
+        @Override
+        public void run() {
+
+        }
     }
 }
