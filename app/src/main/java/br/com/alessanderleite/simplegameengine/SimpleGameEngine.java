a16b2adf22f3fed4768f9e93bb8b92cd51093d4b
alessanderleite <alessander.logistica@gmail.com>
2019-01-01 18:26:28 -0200

    code the run method in the inner class

--- app/src/main/java/br/com/alessanderleite/simplegameengine/SimpleGameEngine.java
+++ app/src/main/java/br/com/alessanderleite/simplegameengine/SimpleGameEngine.java
@@ -85,7 +85,25 @@ public class SimpleGameEngine extends Activity {
         // GameView class will go here
         @Override
         public void run() {
+            while (playing) {
 
+                // Capture the current time in milliseconds in startFrameTime
+                long startFrameTime = System.currentTimeMillis();
+
+                // Update the frame
+                update();
+
+                // Draw the frame
+                draw();
+
+                // Calculate the fps this frame
+                // We can then use the result to
+                // time animations and more.
+                timeThisFrame = System.currentTimeMillis() - startFrameTime;
+                if (timeThisFrame > 0) {
+                    fps = 1000 / timeThisFrame;
+                }
+            }
         }
     }
 }
