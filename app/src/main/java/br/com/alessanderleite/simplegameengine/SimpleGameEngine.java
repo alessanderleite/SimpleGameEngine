30156b5a13db6cd00dfb22e8c7a6df8500cdb5e7
alessanderleite <alessander.logistica@gmail.com>
2019-01-01 11:33:59 -0200

    declare the GameView inner class inside the SimpleGameEngine class and initialize it in the constructor

--- app/src/main/java/br/com/alessanderleite/simplegameengine/SimpleGameEngine.java
+++ app/src/main/java/br/com/alessanderleite/simplegameengine/SimpleGameEngine.java
@@ -7,9 +7,18 @@ import android.view.SurfaceView;
 
 public class SimpleGameEngine extends Activity {
 
+    // gameView will be the view of the game
+    // It will also hold the logic of the game
+    // and respond to screen touches as well
+    GameView gameView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Initialize gameView and set it as the view
+        gameView = new GameView(this);
+        setContentView(gameView);
     }
 
     // Here is our implementation of GameView
@@ -22,6 +31,7 @@ public class SimpleGameEngine extends Activity {
             super(context);
         }
 
+        // GameView class will go here
         @Override
         public void run() {
 
